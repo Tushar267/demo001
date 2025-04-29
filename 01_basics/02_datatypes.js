310f7fb15acda3613d685a999522aebbde91776e
Tushar267 <127430666+Tushar267@users.noreply.github.com>
2025-04-29 11:42:00 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we using node js not browser
+
+console.log(3+3);
+
+let name ="Tushar"
+let age = 23
+let isLoggedIn = false
+let state = null;
+
+console.table([name,age,isLoggedIn,state]);
+console.log(typeof name); // to check the type of data
+
