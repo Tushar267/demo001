c0164347fb74883417f95745a72235a2cd6086a5
Tushar267 <127430666+Tushar267@users.noreply.github.com>
2025-04-29 11:43:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1444553
+let accountEmail = "tusharghadge267@gmail.com"
+var accountPassword = "1234"
+accountcity ="solapur"
+let accountstat;
+
+// accountId = 2  //not allowed
+accountEmail ="tgt"
+accountPassword ="4321"
+account ="pune"
+
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountstat])
